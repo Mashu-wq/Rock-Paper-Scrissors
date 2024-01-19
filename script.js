7f2049dca31b598046fe391bde1bf98f3641e8d0
Mayesha <mim2007088@stud.kuet.ac.bd>
2024-01-19 12:04:46 +0600

    implemented ui

--- script.js
+++ script.js
@@ -40,3 +40,24 @@ function getRandomChoice(){
 function isGameOver(){
     return playerScore === 5 || computerScore === 5;
 }
+//ui design
+
+const scoreInfo = document.getElementById('scoreInfo');
+const scoreMessage = document.getElementById('scoreMessage');
+const playerScorePara = document.getElementById('playerScore');
+const computerScorePara = document.getElementById('computerScore');
+const playerSign = document.getElementById('playerSign');
+const computerSign = document.getElementById('computerSign');
+const rockBtn = document.getElementById('rockBtn');
+const paperBtn = document.getElementById('paperBtn');
+const scissorsBtn = document.getElementById('scissorsBtn');
+const endgameModal = document.getElementById('endgameModal');
+const endgameMsg = document.getElementById('endgameMsg');
+const overlay = document.getElementById('overlay');
+const restartBtn = document.getElementById('restartBtn');
+
+rockBtn.addEventListener('click', () => handleClick('ROCK'));
+paperBtn.addEventListener('click', () => handleClick('PAPER'));
+scissorsBtn.addEventListener('click', () => handleClick('SCISSORS'));
+restartBtn.addEventListener('click', restartGame);
+overlay.addEventListener('click', closeEndgameModal);
